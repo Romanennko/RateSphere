d22d80c24af10e9e4382dbcbdd01a3ef24180038
Mykyta <nikiromanen@gmail.com>
2025-04-10 17:43:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivymd.app import MDApp
+
+class RateSphere(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+if __name__ == "__main__":
+    RateSphere().run()

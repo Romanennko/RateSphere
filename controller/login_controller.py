6c20e6fc6f61e2f98dfe9a095ba9838db285e1ad
Mykyta <nikiromanen@gmail.com>
2025-04-11 20:47:20 +0200

    Initial controller for LoginScreen

--- controller/login_controller.py
+++ controller/login_controller.py
@@ -1,20 +1,35 @@
-# mvc_multiscreen_app/model/session_model.py
+import bcrypt
 
-class SessionModel:
-    """Хранит информацию о текущей сессии (залогиненном пользователе)."""
-    def __init__(self):
-        self._logged_in_username = None
+class LoginController:
+    def __init__(self, models, view, app):
+        self.data_model = models['database']    # Link to DatabaseModel
+        self.session_model = models['session']  # Link to SessionModel
+        self.view = view                        # Link to LoginScreen
+        self.app = app                          # Link to the main application class (for navigation)
 
-    def login(self, username):
-        """Сохраняет имя пользователя при входе."""
-        self._logged_in_username = username
-        print(f"SessionModel: Пользователь '{username}' вошел в систему.")
+    def do_login(self, username, password):
+        """Processes a user login attempt."""
+        self.view.show_error("")
 
-    def logout(self):
-        """Сбрасывает имя пользователя при выходе."""
-        print(f"SessionModel: Пользователь '{self._logged_in_username}' вышел.")
-        self._logged_in_username = None
+        if not username or not password:
+            self.view.show_error("Please fill in all the fields")
+            return
 
-    def get_logged_in_username(self):
-        """Возвращает имя текущего пользователя или None."""
-        return self._logged_in_username
+        user_data = self.data_model.get_user_by_username(username)
+
+        if user_data:
+            stored_user_id, stored_username, stored_email, stored_hash = user_data
+
+            if bcrypt.checkpw(password.encode('utf-8'), stored_hash.encode('utf-8')):
+                self.session_model.login(stored_username, stored_user_id)
+                print(f"User '{username}' logged in successfully.")
+                self.app.screen_manager.current = "ratings"
+            else:
+                self.view.show_error("Invalid username or password")
+        else:
+            self.view.show_error("Invalid username or password")
+
+    def go_to_signup(self):
+        """Transition to the registration screen."""
+        self.view.show_error("")
+        self.app.screen_manager.current = "signup"

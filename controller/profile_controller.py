89b9cef5a9c7f8bd7ec8004b122f0a2c42bc8472
Mykyta <nikiromanen@gmail.com>
2025-04-11 20:48:16 +0200

    Initial controller for ProfileScreen

---
+++ controller/profile_controller.py
@@ -0,0 +1,6 @@
+class ProfileController:
+    def __init__(self, models, view, app):
+        self.data_model = models['database']    # Link to DatabaseModel
+        self.session_model = models['session']  # Link to SessionModel
+        self.view = view                        # Link to ProfileScreen
+        self.app = app                          # Link to the main application class (for navigation)

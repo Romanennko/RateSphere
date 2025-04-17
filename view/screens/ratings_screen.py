f3a81db19dadac4d48b9a035ab1dda5d6883ed9f
Mykyta <nikiromanen@gmail.com>
2025-04-17 16:52:47 +0200

    Added an error check in the show_error function

--- view/screens/ratings_screen.py
+++ view/screens/ratings_screen.py
@@ -3,4 +3,7 @@ from kivymd.uix.screen import MDScreen
 class RatingsScreen(MDScreen):
     def show_error(self, message):
         """Displays an error message."""
-        self.ids.error_label.text = message
+        try:
+            self.ids.error_label.text = message
+        except KeyError:
+             print(f"RatingsScreen: No id 'error_label' for message: {message}")

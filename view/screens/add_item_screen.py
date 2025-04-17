db015fb8797b0fd87ff087071ab4afb3f3e522ea
Mykyta <nikiromanen@gmail.com>
2025-04-17 21:43:59 +0200

    Added functionality for the screen

--- view/screens/add_item_screen.py
+++ view/screens/add_item_screen.py
@@ -1,6 +1,52 @@
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.menu import MDDropdownMenu
+
+ITEM_STATUSES = ['Completed', 'In Progress', 'Planned', 'Dropped', 'Ongoing']
 
 class AddItemScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rating = None
+        self.status_menu = None
+        self.status_menu_items = self._create_menu_items(ITEM_STATUSES, self._set_status)
+
+    def clear_fields(self):
+        self.ids.item_name.text = ''
+        self.ids.item_alt_name.text = ''
+        self.ids.item_type.text = ''
+        self.ids.status_button_text.text = "Select status"
+        self.rating = None
+        self.ids.item_review.text = ''
+
+    def _create_menu_items(self, items_list, callback):
+        return [
+            {
+                "text": item,
+                "on_release": lambda x=item: callback(x),
+            } for item in items_list
+        ]
+
+    def open_status_menu(self, caller_widget):
+        # TODO [WARNING] Deprecated property "<NumericProperty name=padding_x>" of object "<kivymd.uix.label.label.MDLabel object at 0x0000021DE052D710>" was accessed, it will be removed in a future version
+        if not self.status_menu:
+            self.status_menu = MDDropdownMenu(
+                caller=caller_widget,
+                items=self.status_menu_items,
+            )
+        self.status_menu.caller = caller_widget
+        self.status_menu.open()
+
+    def _set_status(self, selected_status):
+        self.ids.status_button_text.text = selected_status
+        if self.status_menu:
+            self.status_menu.dismiss()
+
+    def on_rating_change(self, rating):
+        self.rating = rating
+
+    def get_rating(self):
+        return self.rating
+
     def show_error(self, message):
         """Displays an error message."""
         self.ids.error_label.text = message
